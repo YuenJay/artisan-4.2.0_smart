f25d617d5d6bcf21613775ea8f7e695b41877eec
jay <yuanjie1853@foxmail.com>
2026-09-12 04:46:12 +0800

    Initial commit

---
+++ src/artisanlib/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '4.2.0'
+__revision__ = ''
+__build__ = '0'
+__artisan_os__ = 'Linux'
+
+__release_sponsor_name__ = 'LABEL!ISTEN'
+__release_sponsor_domain__ = 'labelisten.com'
+__release_sponsor_url__ = 'https://labelisten.com/'
+__signature__ = '29f13f2dbeacd4b04901ccd9a8848c5160d74ecaefdaf4fa565347cfdd820eeac3f92392ccc847f5bb5f58c08ad39be16514a7279141913f0572664b21591b0c'
